c2b4f2af572bf31782a218bc73f67a8dbced23fb
ryota <kurokawa.ryota.1124@gmail.com>
2024-03-02 02:37:06 +0900

    initialization

---
+++ joppy/joppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  joppyApp.swift
+//  joppy
+//
+//  Created by 黒川良太 on 2024/03/01.
+//
+
+import SwiftUI
+
+@main
+struct joppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
